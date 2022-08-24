cf1b5fc0053d983737f8b6865108a7b071cd439e
19az <azezin@inbox.ru>
2022-08-24 20:39:20 +0300

    Added documentary

--- doc/html/search/all_2.js
+++ doc/html/search/all_2.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['se_5fsolve_0',['se_solve',['../SESolver_8c.html#a2f60bba9a49acf5cb0b71341c64f5bcb',1,'se_solve(double a, double b, double c, double *root1, double *root2):&#160;SESolver.c'],['../SESolver_8h.html#a2f60bba9a49acf5cb0b71341c64f5bcb',1,'se_solve(double a, double b, double c, double *root1, double *root2):&#160;SESolver.c']]],
   ['se_5fsolve_5fut_1',['se_solve_ut',['../SESolverUT_8c.html#a901d825bbdc0f3c1ad622d3c04c66eef',1,'se_solve_ut():&#160;SESolverUT.c'],['../SESolverUT_8h.html#a901d825bbdc0f3c1ad622d3c04c66eef',1,'se_solve_ut():&#160;SESolverUT.c']]],
-  ['sesolver_2ec_2',['SESolver.c',['../SESolver_8c.html',1,'']]],
-  ['sesolver_2eh_3',['SESolver.h',['../SESolver_8h.html',1,'']]],
-  ['sesolverut_2ec_4',['SESolverUT.c',['../SESolverUT_8c.html',1,'']]],
-  ['sesolverut_2eh_5',['SESolverUT.h',['../SESolverUT_8h.html',1,'']]]
+  ['sesolver_2',['SESolver',['../md_README.html',1,'']]],
+  ['sesolver_2ec_3',['SESolver.c',['../SESolver_8c.html',1,'']]],
+  ['sesolver_2eh_4',['SESolver.h',['../SESolver_8h.html',1,'']]],
+  ['sesolverut_2ec_5',['SESolverUT.c',['../SESolverUT_8c.html',1,'']]],
+  ['sesolverut_2eh_6',['SESolverUT.h',['../SESolverUT_8h.html',1,'']]]
 ];
---
+++ doc/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['sesolver_0',['SESolver',['../md_README.html',1,'']]]
+];
